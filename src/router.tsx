af4dc8f01fb13b1df5a6a42947436729277f89fc
Gustavo Saraiva <gsaraivam10@gmail.com>
2026-03-31 16:38:41 -0300

    correção github workflow pt3

--- src/router.tsx
+++ src/router.tsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom'
 import App from './App'
-import { Home } from '@/pages/Home'
-import { About } from '@/pages/About'
+import { Home } from './pages/Home'
+import { About } from './pages/About'
 
 export const router = createBrowserRouter([
   {
