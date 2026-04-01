8a5946d789a868362065fa73a3cf51170195cdb5
Gustavo Saraiva <gsaraivam10@gmail.com>
2026-04-01 12:43:33 -0300

    alteração no vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,16 @@ import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import path from 'path/win32'
-
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/my-health/',
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({ presets: [reactCompilerPreset(),
+    tsconfigPaths()
+    ] })
   ],
   resolve: {
     alias: {
